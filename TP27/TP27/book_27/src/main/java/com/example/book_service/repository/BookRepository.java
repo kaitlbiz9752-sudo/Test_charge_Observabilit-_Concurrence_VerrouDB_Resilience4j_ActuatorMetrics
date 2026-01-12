37cc5ca0de6a739ad02cb5744f44995ec858e274
kaitlbiz9752-sudo <k.aitlbiz9752@uca.ac.ma>
2026-01-12 02:37:10 +0100

    Add files via upload

---
+++ TP27/TP27/book_27/src/main/java/com/example/book_service/repository/BookRepository.java
@@ -0,0 +1,19 @@
+package com.example.book_service.repository;
+
+import com.example.book_service.domain.Book;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByTitle(String title);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select b from Book b where b.id = :id")
+    Optional<Book> findByIdForUpdate(@Param("id") Long id);
+}
---
+++ TP27/TP27/pricing_27/src/main/java/com/example/pricing_service/web/PricingController.java
@@ -0,0 +1,29 @@
+package com.example.pricing_service.web;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController
+@RequestMapping("/api/prices")
+public class PricingController {
+
+    @GetMapping("/{bookId}")
+    public double price(@PathVariable long bookId,
+                        @RequestParam(name = "fail", defaultValue = "false") boolean fail) {
+
+        // 1) panne  tests
+        if (fail) {
+            throw new IllegalStateException("Pricing down (forced)");
+        }
+
+        // 2) panne pour simuler instabilité
+        if (ThreadLocalRandom.current().nextInt(100) < 30) {
+            throw new IllegalStateException("Random failure");
+        }
+
+        // 3) prix stable
+        return 50.0 + (bookId % 10) * 5.0;
+    }
+}
